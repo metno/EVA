2712a3014ef9a62c9ad76e2a272db8fd3fd9c7c8
Kim Tore Jensen <kimtj@met.no>
2016-01-14 13:21:15 +0100

    Finish the Grid Engine executor class
    Summary: This patch adds support for getting Grid Engine job exit status using `qacct`, and setting the stdout and stderr members of the `Job` object.
    
    Test Plan: N/A
    
    Reviewers: #ecdiss, havardf
    
    Reviewed By: #ecdiss, havardf
    
    Projects: #ecdiss
    
    Maniphest Tasks: T2159
    
    Differential Revision: https://phab.met.no/D1417

--- eva/executor/gridengine.py
+++ eva/executor/gridengine.py
@@ -10,7 +10,7 @@ import eva.job
 
 STATE_DIRECTORY = '/var/lib/eva'
 
-QSUB_EXIT_OK = 0
+EXIT_OK = 0
 QSUB_EXIT_NO_FREE_RESOURCES = 25
 
 
@@ -21,11 +21,36 @@ def get_std_lines(std):
     return std.splitlines() if std is not None else []
 
 
+def strip_stdout_newlines(lines):
+    """
+    Strip newlines from an array of strings.
+    """
+    return [line.strip() for line in lines]
+
+
 def get_job_id_from_qsub_output(output):
+    """
+    Parse the JOB_ID from qsub output. Unfortunately, there is no command-line
+    option to make it explicitly machine readable, so we use a regular
+    expression to extract the first number instead.
+    """
     matches = re.search('\d+', output)
     if not matches:
         raise Exception('Unparseable output from qsub: expected job id, but no digits in output: %s' % output)
     return int(matches.group(0))
+
+
+def get_exit_code_from_qacct_output(output):
+    """
+    @brief Parse a finished job's exit code using the output from qacct.
+    @returns Exit code if the job is finished, or None if it is still running.
+    """
+    regex = re.compile('^exit_status\s+(\d+)\s*$')
+    for line in output:
+        matches = regex.match(line)
+        if matches:
+            return int(matches.group(1))
+    return None
 
 
 def generate_job_script(job):
@@ -42,14 +67,18 @@ class GridEngineExecutor(eva.executor.BaseExecutor):
     """
 
     def execute_async(self, job):
+
+        # Create a temporary submit script
         with tempfile.NamedTemporaryFile(mode='rw+b') as submit_script:
             script_content = generate_job_script(job)
             submit_script.write(script_content)
-            stdout_path = get_output_path('$JOB_ID.stdout')
-            stderr_path = get_output_path('$JOB_ID.stderr')
+            job.stdout_path = get_output_path('$JOB_ID.stdout')
+            job.stderr_path = get_output_path('$JOB_ID.stderr')
+
+            # Submit the job using qsub
             command = ['qsub', '-b', 'n',
-                       '-o', stdout_path,
-                       '-e', stderr_path,
+                       '-o', job.stdout_path,
+                       '-e', job.stderr_path,
                        submit_script.name,
                        ]
             logging.info('[%s] Executing: %s' % (job.id, ' '.join(command)))
@@ -57,13 +86,18 @@ class GridEngineExecutor(eva.executor.BaseExecutor):
             stdout, stderr = process.communicate()
             exit_code = process.returncode
             logging.info('[%s] Exit status: %d' % (job.id, exit_code))
-            if exit_code == QSUB_EXIT_OK:
+
+            # Job was submitted. Set paths, job id, status, and return
+            if exit_code == EXIT_OK:
                 job.set_status(eva.job.STARTED)
                 job.pid = get_job_id_from_qsub_output(get_std_lines(stdout)[0])
+                job.stdout_path = job.stdout_path.replace('$JOB_ID', unicode(job.pid))
+                job.stderr_path = job.stderr_path.replace('$JOB_ID', unicode(job.pid))
                 logging.info('[%s] Grid Engine JOB_ID: %d' % (job.id, job.pid))
                 return
-            else:
-                job.set_status(eva.job.FAILED)
+
+            # Submitting the job failed. Do some verbose logging.
+            job.set_status(eva.job.FAILED)
             if exit_code == QSUB_EXIT_NO_FREE_RESOURCES:
                 logging.error('[%s] Job failed to start because there are no free Grid Engine resources at the moment. Please try again later.', (job.id))
             else:
@@ -80,11 +114,29 @@ class GridEngineExecutor(eva.executor.BaseExecutor):
             logging.debug('[%s] --- End of script file contents ---', (job.id))
 
     def update_status(self, job):
+
+        # Check job status using qacct
         command = ['qacct', '-j', unicode(job.pid)]
         logging.info('[%s] Executing: %s' % (job.id, ' '.join(command)))
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         exit_code = process.returncode
         logging.info('[%s] Exit status: %d' % (job.id, exit_code))
-        # FIXME
-        job.set_status(eva.job.COMPLETE)
+        if exit_code != 0:
+            logging.error('[%s] qacct failed, so I was unable to retrieve the job status.', (job.id))
+            return
+        exit_code = get_exit_code_from_qacct_output(get_std_lines(stdout))
+        if exit_code is None:
+            return
+
+        # Job is finished. Assign exit code, stdout and stderr to the Job object.
+        logging.info('[%s] Grid Engine job %d finished with exit status %d' % (job.id, job.pid, exit_code))
+        job.exit_code = exit_code
+        if exit_code == EXIT_OK:
+            job.set_status(eva.job.COMPLETE)
+        else:
+            job.set_status(eva.job.FAILED)
+        with open(job.stdout_path, 'r') as f:
+            job.stdout = strip_stdout_newlines(f.readlines())
+        with open(job.stderr_path, 'r') as f:
+            job.stderr = strip_stdout_newlines(f.readlines())
