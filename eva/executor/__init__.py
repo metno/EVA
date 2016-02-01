a6633d50db499df5ea762262b79fc3941844c7b0
Alexander Bürger <alexanderb@met.no>
2016-02-01 09:50:45 +0100

    bugfix: number of arguments for executor methods
    Summary: bugfix
    
    Test Plan: run EVA using NullExecutor
    
    Reviewers: #ecdiss, kimtj
    
    Reviewed By: #ecdiss, kimtj
    
    Subscribers: #ecdiss
    
    Differential Revision: https://phab.met.no/D1494

--- eva/executor/__init__.py
+++ eva/executor/__init__.py
@@ -11,13 +11,13 @@ class BaseExecutor(object):
     def __init__(self, environment_variables):
         self.env = environment_variables
 
-    def execute_async(self, adapter, job):
+    def execute_async(self, job):
         """
         Execute the job asynchronously, and return immediately.
         """
         raise NotImplementedError()
 
-    def update_status(self, adapter, job):
+    def update_status(self, job):
         """
         Check job status, and update the Job object accordingly, populating
         Job.exit_code, Job.stdout, Job.stderr, and Job.status.
@@ -44,10 +44,10 @@ class NullExecutor(BaseExecutor):
     Pretend to execute tasks, but don't actually do it.
     """
 
-    def execute_async(self, adapter, job):
+    def execute_async(self, job):
         job.status = eva.job.STARTED
 
-    def update_status(self, adapter, job):
+    def update_status(self, job):
         job.status = eva.job.COMPLETE
 
 
