cd05fb826374cfbc4f3e7b98d2d532be8237ef85
Kim Tore Jensen <kimtj@met.no>
2016-09-27 15:55:13 +0200

    FimexAdapter: use generate_resources() API for creating resources that should be posted

--- eva/adapter/fimex.py
+++ eva/adapter/fimex.py
@@ -6,8 +6,6 @@ import eva.job
 import eva.exceptions
 import eva.template
 
-import productstatus
-
 
 class FimexAdapter(eva.base.adapter.BaseAdapter):
     """!
@@ -70,11 +68,10 @@ class FimexAdapter(eva.base.adapter.BaseAdapter):
         @return True if all optional output variables are configured, False otherwise.
         """
         return (
-            (self.env['EVA_OUTPUT_BASE_URL'] is not None) and
-            (self.env['EVA_OUTPUT_DATA_FORMAT'] is not None) and
-            (self.env['EVA_OUTPUT_LIFETIME'] is not None) and
-            (self.env['EVA_OUTPUT_PRODUCT'] is not None) and
-            (self.env['EVA_OUTPUT_SERVICE_BACKEND'] is not None)
+            (len(self.env['EVA_OUTPUT_BASE_URL']) > 0) and
+            (len(self.env['EVA_OUTPUT_DATA_FORMAT']) > 0) and
+            (len(self.env['EVA_OUTPUT_PRODUCT']) > 0) and
+            (len(self.env['EVA_OUTPUT_SERVICE_BACKEND']) > 0)
         )
 
     def create_job(self, message_id, resource):
@@ -121,78 +118,45 @@ class FimexAdapter(eva.base.adapter.BaseAdapter):
         if not self.post_to_productstatus:
             return
 
-        # Post a new ProductInstance resource
-        self.logger.info('Creating a new ProductInstance resource on the Productstatus server...')
-        productinstance = eva.retry_n(
-            self.post_productinstance_resource,
-            args=(self.output_product, job.reference_time),
-            exceptions=(productstatus.exceptions.ServiceUnavailableException,),
-            give_up=30,
-        )
+        job.logger.info('Generating Productstatus resources...')
+        resources = self.generate_resources(job)
+        self.post_resources(resources, job)
+        job.logger.info('Finished posting to Productstatus; all complete.')
 
-        # Post a new Data resource
-        self.logger.info('Creating a new Data resource on the Productstatus server...')
-        data = eva.retry_n(
-            self.get_or_post_data_resource,
-            args=(productinstance, None, None),
-            exceptions=(
-                productstatus.exceptions.ServiceUnavailableException,
-            ),
-            give_up=30,
-        )
-
-        # Post a new DataInstance resource
-        self.logger.info('Creating a new DataInstance resource on the Productstatus server...')
-        url = os.path.join(self.env['EVA_OUTPUT_BASE_URL'], os.path.basename(job.output_filename))
-        datainstance = eva.retry_n(
-            self.post_datainstance_resource,
-            args=(
-                data,
-                self.expiry_from_lifetime(),
-                self.output_data_format,
-                self.output_service_backend,
-                url,
-            ),
-            exceptions=(productstatus.exceptions.ServiceUnavailableException,),
-            give_up=30,
-        )
-
-        # All records written, log success
-        self.logger.info('DataInstance %s, expires %s', datainstance, datainstance.expires)
-        self.logger.info('The file %s has been successfully generated', datainstance.url)
-
-    def post_productinstance_resource(self, product, reference_time):
-        """!
-        @brief Create a ProductInstance resource.
-        """
-        productinstance = self.api.productinstance.create()
-        productinstance.product = product
-        productinstance.reference_time = reference_time
-        productinstance.save()
-        return productinstance
-
-    def get_or_post_data_resource(self, productinstance, time_period_begin, time_period_end):
-        """!
-        @brief Return a matching Data resource according to ProductInstance and
-        data file begin/end times.
-        """
-        parameters = {
-            'productinstance': productinstance,
-            'time_period_begin': time_period_begin,
-            'time_period_end': time_period_end,
+    def generate_resources(self, job):
+        resources = {
+            'productinstance': [],
+            'data': [],
+            'datainstance': [],
         }
-        return self.api.data.find_or_create(parameters)
 
-    def post_datainstance_resource(self, data, expires, format, servicebackend, url):
-        """!
-        @brief Create a DataInstance resource at the Productstatus server,
-        referring to the given data set.
-        """
-        resource = self.api.datainstance.create()
-        resource.data = data
-        resource.expires = expires
-        resource.format = format
-        resource.servicebackend = servicebackend
-        resource.url = url
-        resource.save()
-        return resource
+        # Generate ProductInstance resource
+        parameters = {
+            'product': self.output_product,
+            'reference_time': job.resource.data.productinstance.reference_time,
+        }
+        if self.output_product == job.resource.data.productinstance.product:
+            parameters['version'] = job.resource.data.productinstance.version
+
+        product_instance = self.api.productinstance.find_or_create_ephemeral(parameters)
+        resources['productinstance'] += [product_instance]
+
+        # Generate Data resource
+        data = self.api.data.find_or_create_ephemeral({
+            'productinstance': product_instance,
+            'time_period_begin': job.resource.data.time_period_begin,
+            'time_period_end': job.resource.data.time_period_end,
+        })
+        resources['data'] += [data]
+
+        # Generate DataInstance resource
+        datainstance = self.api.datainstance.find_or_create_ephemeral({
+            'data': data,
+            'expires': self.expiry_from_lifetime(),
+            'format': self.output_data_format,
+            'servicebackend': self.output_service_backend,
+            'url': os.path.join(self.env['EVA_OUTPUT_BASE_URL'], os.path.basename(job.output_filename)),
+        })
+        resources['datainstance'] += [datainstance]
+
+        return resources
