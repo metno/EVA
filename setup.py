1f4c8babd04ca629a731ed79b75a2522320a407f
Kim Tore Jensen <kimtj@met.no>
2016-11-07 14:10:28 +0100

    Document Semantic Versioning in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+#
+# Follows Semantic Versioning, as described here: http://semver.org/
+#
+# Given a version number MAJOR.MINOR.PATCH, increment the:
+#
+# MAJOR version when you make incompatible API changes,
+# MINOR version when you add functionality in a backwards-compatible manner, and
+# PATCH version when you make backwards-compatible bug fixes.
+#
+# Additional labels for pre-release and build metadata are available as
+# extensions to the MAJOR.MINOR.PATCH format.
+#
+
+VERSION = [1, 0, 0]
 
 config = {
     'description': 'Event Adapter',
@@ -6,7 +20,7 @@ config = {
     'url': 'https://github.com/metno/productstatus-eva',
     'download_url': 'https://github.com/metno/productstatus-eva',
     'author_email': 'it-geo-tf@met.no',
-    'version': '1.0',
+    'version': '.'.join([str(x) for x in VERSION]),
     'install_requires': [
         'nose==1.3.7',
         'python-dateutil==2.5.0',
