6ed55bce18873cf36c8ec82fe5fb29a66b31614a
Kim Tore Jensen <kimtj@met.no>
2016-11-18 12:01:36 +0100

    Fix import tests

---
+++ eva/tests/test_import.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestImport(unittest.TestCase):
+    def test_import_adapter(self):
+        import eva.adapter
+
+    def test_import_executor(self):
+        import eva.executor
+
+    def test_import_listener(self):
+        import eva.listener
